d0fe9f2d04d790d4718b82d8800208443d375e6f
murilo-a-s <muu.anjos@gmail.com>
2025-06-26 20:58:37 -0300

    forca melhoras
    em processo

--- forcabackup.py
+++ forcabackup.py
@@ -1,13 +1,15 @@
-#funções a
-def game_start():
-    secret_word = 'ABACAXI'
-    lives = 6
-    MAX_LIVES = 6
-    game_won = False
-    game_lost = False
-    incorrect_guessed_letters = []
-    display_word = []
-    hangman_stages = ["""
+secret_word = 'ABACAXI'
+lives = 6
+MAX_LIVES = 6
+game_won = False
+game_lost = False
+incorrect_guessed_letters = []
+display_word = []
+
+for letter in secret_word:
+    display_word.append('_')
+
+hangman_stages = ["""
              +---+
              |   |
                  |
@@ -70,17 +72,31 @@ def game_start():
                  |
           =========
           """]
-    
-    for letter in secret_word:
-        display_word.append('_')
-    
-    print('X===BEM=VINDO=AO=JOGO=DA=FORCA===X','\r\n')
-    
-    return secret_word, lives, MAX_LIVES, game_won, game_lost, incorrect_guessed_letters, display_word, hangman_stages
-    
+
+#funções Game
 def input_player_guess():
+    player_input = input('Digite uma letra: ')
+    return player_input
+
+
+def display_game_state(display_word, lives, incorrect_guessed_letters, hangman_stages):
+    print(f"Palavra: {' '.join(display_word)}", '\r\n')
+
+    print(f'Vidas Restantes: {lives}', '\r\n')
+
+    print(f'Tentativas Incorretas: {', '.join(incorrect_guessed_letters)}', '\r\n')
+
+    print(hangman_stages[MAX_LIVES - lives])
+    
+print('X===BEM=VINDO=AO=JOGO=DA=FORCA===X','\r\n')
+
+display_game_state(display_word, lives, incorrect_guessed_letters, hangman_stages)
+
+#Game Loop
+while lives > 0 and not game_won:
+    #Turno tentativa do jogador
     while True:
-        player_input = input('Digite uma letra: ').strip()
+        player_input = input_player_guess()
         guess = player_input.upper()
 
         # checando se é 1 caracter somente
@@ -97,9 +113,8 @@ def input_player_guess():
             continue
         # a letra é válida, quebre o loop
         print(f'Você digitou {guess}!')
-        return guess
-    
-def check_guess(guess, secret_word, display_word, lives, incorrect_guessed_letters):
+        break
+
     # checando se a letra está na palavra secreta    
     if guess in secret_word: #true
         print(f'Parabéns! {guess} está na palavra secreta!','\r\n')
@@ -110,30 +125,7 @@ def check_guess(guess, secret_word, display_word, lives, incorrect_guessed_lette
         print(f'Desculpe, {guess} não está na plavra secreta!','\r\n')
         lives -= 1
         incorrect_guessed_letters.append(guess)
-    return lives, display_word, incorrect_guessed_letters
-    
 
-def display_game_state(display_word, lives, incorrect_guessed_letters, hangman_stages):
-    print(f"Palavra: {' '.join(display_word)}", '\r\n')
-
-    print(f'Vidas Restantes: {lives}', '\r\n')
-
-    print(f'Tentativas Incorretas: {', '.join(incorrect_guessed_letters)}', '\r\n')
-
-    print(hangman_stages[MAX_LIVES - lives])
-    
-    
-secret_word, lives, MAX_LIVES, game_won, game_lost, incorrect_guessed_letters, display_word, hangman_stages = game_start()
-
-display_game_state(display_word, lives, incorrect_guessed_letters, hangman_stages)
-
-
-#Game Loop
-while lives > 0 and not game_won:
-    #Turno tentativa do jogador
-    guess = input_player_guess()
-    # checando se a letra está na palavra secreta    
-    lives, display_word, incorrect_guessed_letters = check_guess(guess, secret_word, display_word, lives, incorrect_guessed_letters) 
     #game board
     display_game_state(display_word, lives, incorrect_guessed_letters, hangman_stages)
     
